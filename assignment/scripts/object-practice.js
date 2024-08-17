79f7c499c33f10a32deec3222aa2ce39ef9e1db6
Jenny Lagervall <jennylagervall@gmail.com>
2024-08-17 15:53:16 -0700

    worked through item 1 -3

--- assignment/scripts/object-practice.js
+++ assignment/scripts/object-practice.js
@@ -8,7 +8,12 @@ console.log('***** Object Practice *****')
   - Give it  a numeric 'shoeCount' property
   - Also give it an array 'favThreeFoods'
 */
-const me = {
+let me = {
+  firstName: 'Jenny',
+  lastName: 'Lagervall',
+  hasSiblings: true,
+  shoeCount: 7,
+  favThreeFoods:['tacos', 'indian', 'salad']
   // TODO - add properties here
 };
 console.log('A little about me:', me);
@@ -19,27 +24,31 @@ console.log('A little about me:', me);
     created above to set its value. 
   - Console.log fullName
 */
+const fullName = `${me.firstName} ${me.lastName}`;
+console.log('my full name is:', fullName);
+
+// /* 3. Nested arrays
+//   - Console.log your first favorite food
+//   - Console.log your last favorite food 
+// */
+
+console.log('my first favorite food is:', me.favThreeFoods[0]);
+console.log('my third favorite food is:', me.favThreeFoods[2]);
+
+// /* 4. Change a property of an existing object.
+//   - Console.log your current shoe count.
+//   - Guess what?! You just got a new pair of shoes!!!
+//   - Set your object's shoeCount to be what it was plus 1
+//   - Console.log your updated shoe count. 
+// */
+console.log('my current shoe count is:', me.shoeCount)
+me.shoeCount.push
+console.log('my new shoe count is:', me.shoeCount)
 
 
-
-/* 3. Nested arrays
-  - Console.log your first favorite food
-  - Console.log your last favorite food 
-*/
-
-
-
-/* 4. Change a property of an existing object.
-  - Console.log your current shoe count.
-  - Guess what?! You just got a new pair of shoes!!!
-  - Set your object's shoeCount to be what it was plus 1
-  - Console.log your updated shoe count. 
-*/
-
-
-/* 5. Add a new property to an existing object.
-  - Add a 'favoriteColor' to your existing object
-  - Do this the same way you updated the variable above
-  - Cool huh? It works, even though it wasn't there before
-  - Console.log your object
-*/
+// /* 5. Add a new property to an existing object.
+//   - Add a 'favoriteColor' to your existing object
+//   - Do this the same way you updated the variable above
+//   - Cool huh? It works, even though it wasn't there before
+//   - Console.log your object
+// */
